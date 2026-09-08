f49b804bcd29e3c6a97c88fcf57f60182ec5a1b0
Lucas Alberto <lucas.a.santos126@aluno.senai.br>
2026-09-08 20:37:20 -0300

    Criado o model Pessoa

---
+++ src/app/models/pessoa.ts
@@ -0,0 +1,10 @@
+export class Pessoa {
+    idpessoa: number = 0
+    nome: string = ''
+    cpf: string = ''
+    data_nascimento: string = ''
+    sexo: string = ''
+    telefone: string = ''
+    email: string = ''
+    senha: string = ''
+}
