125ff152a3d11f1eac0c774043b666e102f3efb4
Lucas Alberto <lucas.a.santos126@aluno.senai.br>
2026-09-08 20:57:38 -0300

    Adicionados os caminhos para os components home, menu e carrihno

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home-component/home-component';
+import { PessoaCadastro } from './pessoa-cadastro/pessoa-cadastro';
+import { Carrinho } from './carrinho/carrinho';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        redirectTo: "home",
+        pathMatch: "full"
+    },
+    {
+        path: "home",
+        component: HomeComponent
+    },
+    {
+        path: "cadastrar",
+        component: PessoaCadastro
+    },
+    {
+        path: "carrinho",
+        component: Carrinho
+    }
+];
