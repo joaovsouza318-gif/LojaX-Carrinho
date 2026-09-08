438108d06a967925341abf2a0b766435e9dd07eb
Lucas Alberto <lucas.a.santos126@aluno.senai.br>
2026-09-08 20:58:59 -0300

    Criado o component menu

---
+++ src/app/menu/menu.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  imports: [RouterLink],
+  selector: 'app-menu',
+  styleUrl: './menu.css',
+  templateUrl: './menu.html',
+})
+export class Menu {}
