8ea59303968460e85711e4b12808c1b64f1d87e1
Pamela Carvalho <pamelakaroline004@gmail.com>
2026-09-08 20:33:30 -0300

    Criando o model de Produto

---
+++ src/app/models/produto.ts
@@ -0,0 +1,9 @@
+export class Produto{
+    idproduto: number = 0;
+    idsetor: number = 0;
+    produto: string = '';
+    descricao_produto: string = '';
+    valor_unitario:number = 0;
+    unidade:number = 0;
+    estoque: number = 0;
+}
