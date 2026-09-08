dac9ec53154dcb0af0463fd0b269c893465e4cda
Lucas Alberto <lucas.a.santos126@aluno.senai.br>
2026-09-08 20:38:24 -0300

    Criado o component pessoaCadastro

---
+++ src/app/pessoa-cadastro/pessoa-cadastro.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  imports: [FormsModule],
+  selector: 'app-pessoa-cadastro',
+  styleUrl: './pessoa-cadastro.css',
+  templateUrl: './pessoa-cadastro.html',
+})
+export class PessoaCadastro {}
