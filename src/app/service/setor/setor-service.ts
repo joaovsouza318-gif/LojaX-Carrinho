0480ffd32dad2898138a83de93b60f57ea0f75ac
antonio francisco <antonio.f.neto36@aluno.senai.br>
2026-09-09 19:48:05 -0300

    criando html e css

--- src/app/service/setor/setor-service.ts
+++ src/app/service/setor/setor-service.ts
@@ -16,24 +16,48 @@ export class SetorService {
     idsetor: 2,
     setor: 'Água',
     descricao_setor: 'Pokémon do tipo Água'
-}
+},
 {
     idsetor: 3,
     setor: 'Planta',
     descricao_setor: 'Pokémon do tipo Planta'
-}
+},
 {
     idsetor: 4,
     setor: 'Elétrico',
     descricao_setor: 'Pokémon do tipo Elétrico'
-}
+},
 {
     idsetor: 5,
     setor: 'Fantasma',
     descricao_setor: 'Pokémon do tipo Fantasma'
-}
+},
 ];
 
-listar
+listar(): Setor[]{
+    return this.setores;
+}
 
+adicionar(setor: Setor): void {
+    const novoId = this.setores.length > 0
+    ? Math.max(...this.setores.map(s => s.idsetor)) + 1
+    : 1;
+
+    setor.idsetor = novoId;
+    this.setores.push(setor);
+}
+
+localizar(id: number): Setor | undefined {
+    return this.setores.find(s => s.idsetor === id);
+}
+
+alterar(setorAlterado: Setor): void {
+    const indice = this.setores.findIndex(
+        s => s.idsetor === setorAlterado.idsetor
+    );
+
+    if (indice !== -1) {
+        this.setores.splice(indice, 1);
+    }
+  }
 }
