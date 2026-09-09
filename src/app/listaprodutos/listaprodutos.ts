a8c9674cf489ed028ca605768923ee64a67f2806
antonio francisco <antonio.f.neto36@aluno.senai.br>
2026-09-09 18:44:34 -0300

    criei a pasta listaprodutos

---
+++ src/app/listaprodutos/listaprodutos.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-listaprodutos',
+  styleUrl: './listaprodutos.css',
+  templateUrl: './listaprodutos.html',
+})
+export class Listaprodutos {}
