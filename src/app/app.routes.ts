ceac4a0c7a34594d8cd81bba340fe817c5b1544a
antonio francisco <antonio.f.neto36@aluno.senai.br>
2026-09-09 20:47:18 -0300

    alterei o routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,9 @@ import { HomeComponent } from './home-component/home-component';
 import { PessoaCadastro } from './pessoa-cadastro/pessoa-cadastro';
 import { Carrinho } from './carrinho/carrinho';
 import { Produtos } from './produtos/produtos';
-<<<<<<< HEAD
-import { SetorComponent } from './setor/setor';
-=======
+import { SetorService } from './service/setor/setor-service';
 import { Listaprodutos } from './listaprodutos/listaprodutos';
->>>>>>> 26e19bd58baf3e1a1154a45fc009303b16a1c134
+
 
 export const routes: Routes = [
     {
@@ -34,8 +32,8 @@ export const routes: Routes = [
     {
         path: "produtos",
         component: Produtos
-    }
+    },
     {
-        path: "setores", component: SetorComponent 
+        path: "setores", component: SetorService 
     }
 ];
