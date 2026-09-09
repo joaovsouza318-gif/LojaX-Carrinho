dcfaedf67e802801a0f63ea8c2f07aebe04f7ec8
Noite Cetafaju <Noite.Cetafaju@labfies.org.br>
2026-09-09 19:05:46 -0300

    Corrigindo imagens inseridas no Carrinho

--- src/app/carrinho/carrinho.ts
+++ src/app/carrinho/carrinho.ts
@@ -1,9 +1,88 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+
+import {
+  CarrinhoService,
+  ItemCarrinho
+} from '../service/carrinho/carrinho-service';
 
 @Component({
-  imports: [],
+  imports: [
+    CommonModule,
+    RouterLink
+  ],
+
   selector: 'app-carrinho',
+
   styleUrl: './carrinho.css',
+
   templateUrl: './carrinho.html',
 })
-export class Carrinho {}
+export class Carrinho {
+
+  frete = 19.90;
+
+  desconto = 0;
+
+
+  constructor(
+    private carrinhoService: CarrinhoService
+  ) {}
+
+
+  get itens(): ItemCarrinho[] {
+    return this.carrinhoService.listar();
+  }
+
+
+  get quantidadeItens(): number {
+    return this.carrinhoService.quantidadeTotal();
+  }
+
+
+  get subtotal(): number {
+    return this.carrinhoService.total();
+  }
+
+
+  get valorFrete(): number {
+
+    if (this.itens.length === 0) {
+      return 0;
+    }
+
+    return this.frete;
+  }
+
+
+  get total(): number {
+
+    return (
+      this.subtotal +
+      this.valorFrete -
+      this.desconto
+    );
+  }
+
+
+  aumentar(item: ItemCarrinho): void {
+    this.carrinhoService.aumentar(item.produto);
+  }
+
+
+  diminuir(item: ItemCarrinho): void {
+    this.carrinhoService.diminuir(item.produto);
+  }
+
+
+  remover(item: ItemCarrinho): void {
+    this.carrinhoService.remover(item.produto);
+  }
+
+
+  limparCarrinho(): void {
+    this.carrinhoService.limpar();
+  }
+
+}
--- src/app/service/carrinho/carrinho-service.ts
+++ src/app/service/carrinho/carrinho-service.ts
@@ -1,32 +1,106 @@
 import { Injectable } from '@angular/core';
 import { Produto } from '../../models/produto';
 
+export interface ItemCarrinho {
+  produto: Produto;
+  quantidade: number;
+}
+
 @Injectable({
   providedIn: 'root',
 })
 export class CarrinhoService {
-  private itens: Produto[] = [];
+
+  private itens: ItemCarrinho[] = [];
 
   adicionar(produto: Produto): void {
-    this.itens.push(produto);
-  }
 
-  remover(produto: Produto): void {
-    const index = this.itens.indexOf(produto);
-    if (index > -1) {
-      this.itens.splice(index, 1);
+    const itemExistente = this.itens.find(
+      item => item.produto.idproduto === produto.idproduto
+    );
+
+    if (itemExistente) {
+
+      if (itemExistente.quantidade < produto.estoque) {
+        itemExistente.quantidade++;
+      }
+
+    } else {
+
+      this.itens.push({
+        produto: produto,
+        quantidade: 1
+      });
+
     }
   }
 
-  listar(): Produto[] {
+
+  aumentar(produto: Produto): void {
+
+    const item = this.itens.find(
+      item => item.produto.idproduto === produto.idproduto
+    );
+
+    if (item && item.quantidade < produto.estoque) {
+      item.quantidade++;
+    }
+  }
+
+
+  diminuir(produto: Produto): void {
+
+    const item = this.itens.find(
+      item => item.produto.idproduto === produto.idproduto
+    );
+
+    if (!item) {
+      return;
+    }
+
+    if (item.quantidade > 1) {
+      item.quantidade--;
+    } else {
+      this.remover(produto);
+    }
+  }
+
+
+  remover(produto: Produto): void {
+
+    this.itens = this.itens.filter(
+      item => item.produto.idproduto !== produto.idproduto
+    );
+  }
+
+
+  listar(): ItemCarrinho[] {
     return this.itens;
   }
+
+
+  quantidadeTotal(): number {
+
+    return this.itens.reduce(
+      (total, item) => total + item.quantidade,
+      0
+    );
+  }
+
+
+  total(): number {
+
+    return this.itens.reduce(
+      (total, item) =>
+        total +
+        (item.produto.valor_unitario * item.quantidade),
+      0
+    );
+  }
+
 
   limpar(): void {
     this.itens = [];
   }
 
-  total(): number {
-    return this.itens.reduce((soma, item) => soma + item.valor_unitario, 0);
-  }
 }
