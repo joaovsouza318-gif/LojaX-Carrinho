69ef6b1f05d48404852c510ffec8c14d4f16bc06
Lucas Alberto <lucas.a.santos126@aluno.senai.br>
2026-09-08 21:36:20 -0300

    formulario para cadastro

--- src/app/pessoa-cadastro/pessoa-cadastro.ts
+++ src/app/pessoa-cadastro/pessoa-cadastro.ts
@@ -7,4 +7,29 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './pessoa-cadastro.css',
   templateUrl: './pessoa-cadastro.html',
 })
-export class PessoaCadastro {}
+export class PessoaCadastro {
+  idpessoa = 0
+  nome = ''
+  cpf = ''
+  data_nascimento = ''
+  sexo = ''
+  telefone = ''
+  email = ''
+  senha = ''
+
+  exibeDados(){
+    console.log(this.idpessoa, this.nome, this.cpf, this.data_nascimento, this.sexo, this.telefone, this.email, this.senha);
+
+    this.limparCampos();
+  }
+
+  limparCampos(){
+    this.nome = ''
+    this.cpf = ''
+    this.data_nascimento = ''
+    this.sexo = ''
+    this.telefone = ''
+    this.email = '',
+    this.senha = ''
+  }
+}
