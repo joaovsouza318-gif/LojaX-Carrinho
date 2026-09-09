033ddb16641a3f33f727172e69ce6f3b9fbecbd9
jonathan <jonathan.a.santos10@aluno.senai.br>
2026-09-08 21:21:16 -0300

    corrigido o erro de nome entre home/menu.

--- src/app/componentes/menu/menu.ts
+++ src/app/componentes/menu/menu.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
+import { RouterModule} from '@angular/router';
 
 @Component({
-  imports: [],
+  imports: [RouterModule],
   selector: 'app-menu',
   styleUrl: './menu.css',
   templateUrl: './menu.html',
