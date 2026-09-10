4bb7b02a063c7fa3ef74a272ceb3e81e96b66eb5
Lucas Alberto <lucas.a.santos126@aluno.senai.br>
2026-09-09 21:21:11 -0300

    Pequena alteração

--- src/app/service/pessoa/pessoa-service.ts
+++ src/app/service/pessoa/pessoa-service.ts
@@ -10,31 +10,31 @@ export class PessoaService {
     constructor (private http: HttpClient) {}
 
     cadastrar(pessoa: Pessoa): Observable<Pessoa> {
-        const urlApi = `http://127.0.0.1:8000/pessoa/`
+        const urlApi = `http://127.0.0.1:8000/pessoas/`
     
         return this.http.post<Pessoa>(urlApi, pessoa)
     }
 
     listarPessoas(): Observable<Pessoa[]>{
-        const urlApi = `http://127.0.0.1:8000/pessoa/`
+        const urlApi = `http://127.0.0.1:8000/pessoas/`
 
         return this.http.get<Pessoa[]>(urlApi)
     }
 
     listarPessoa(pessoaId: number): Observable<Pessoa>{
-        const urlApi = `http://127.0.0.1:8000/pessoa/${pessoaId}`
+        const urlApi = `http://127.0.0.1:8000/pessoas/${pessoaId}`
 
         return this.http.get<Pessoa>(urlApi)
     }
 
     excluirPessoa(pessoa: Pessoa): Observable<Pessoa>{
-        const urlApi = `http://127.0.0.1:8000/pessoa/${pessoa.idpessoa}`
+        const urlApi = `http://127.0.0.1:8000/pessoas/${pessoa.idpessoa}`
 
         return this.http.delete<Pessoa>(urlApi)
     }
 
     alterarPessoa(pessoa: Pessoa): Observable<Pessoa>{
-        const urlApi = `http://127.0.0.1:8000/pessoa/${pessoa.idpessoa}`
+        const urlApi = `http://127.0.0.1:8000/pessoas/${pessoa.idpessoa}`
 
         return this.http.put<Pessoa>(urlApi, pessoa)
     }
