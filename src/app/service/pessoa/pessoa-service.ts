a77f53245ccfaea863eabe960750ed419d4996ae
Lucas Alberto <lucas.a.santos126@aluno.senai.br>
2026-09-10 21:45:11 -0300

    pessoas - link para o fastApi, banco de dados funcional

--- src/app/service/pessoa/pessoa-service.ts
+++ src/app/service/pessoa/pessoa-service.ts
@@ -10,31 +10,31 @@ export class PessoaService {
     constructor (private http: HttpClient) {}
 
     cadastrar(pessoa: Pessoa): Observable<Pessoa> {
-        const urlApi = `https://6aa31e7be7ae868cdf7a9be8.mockapi.io/pessoa/`
+        const urlApi = `http://127.0.0.1:8000/pessoas/`
     
         return this.http.post<Pessoa>(urlApi, pessoa)
     }
 
     listarPessoas(): Observable<Pessoa[]>{
-        const urlApi = `https://6aa31e7be7ae868cdf7a9be8.mockapi.io/pessoa/`
+        const urlApi = `http://127.0.0.1:8000/pessoas/`
 
         return this.http.get<Pessoa[]>(urlApi)
     }
 
     listarPessoa(pessoaId: number): Observable<Pessoa>{
-        const urlApi = `https://6aa31e7be7ae868cdf7a9be8.mockapi.io/pessoa/${pessoaId}`
+        const urlApi = `http://127.0.0.1:8000/pessoas/${pessoaId}`
 
         return this.http.get<Pessoa>(urlApi)
     }
 
     excluirPessoa(pessoa: Pessoa): Observable<Pessoa>{
-        const urlApi = `https://6aa31e7be7ae868cdf7a9be8.mockapi.io/pessoa/${pessoa.idpessoa}`
+        const urlApi = `http://127.0.0.1:8000/pessoas/${pessoa.idpessoa}`
 
         return this.http.delete<Pessoa>(urlApi)
     }
 
     alterarPessoa(pessoa: Pessoa): Observable<Pessoa>{
-        const urlApi = `https://6aa31e7be7ae868cdf7a9be8.mockapi.io/pessoa/${pessoa.idpessoa}`
+        const urlApi = `http://127.0.0.1:8000/pessoas/${pessoa.idpessoa}`
 
         return this.http.put<Pessoa>(urlApi, pessoa)
     }
